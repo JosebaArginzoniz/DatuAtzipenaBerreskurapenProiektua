c79e84e9aeb19e28fa9071fe73aa32d8e7249e7c
JosebaArginzoniz <96474004+JosebaArginzoniz@users.noreply.github.com>
2023-02-17 01:22:50 +0100

    Diskoa Taldea modifikatua

--- 2.Ebal/mongo-diskografia/src/main/java/dambi/accessingmongodiskografia/model/Diskoa.java
+++ 2.Ebal/mongo-diskografia/src/main/java/dambi/accessingmongodiskografia/model/Diskoa.java
@@ -14,88 +14,87 @@ public class Diskoa {
 	public String disketxea;
 	public String mediaCondition;
 	public List<String> abestiak;
-	
-	// Getters
+
 	public int getId() {
-    return id;
+    	return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public Taldea getTaldea() {
 		return taldea;
 	}
 
-	public String getIzena() {
-		return izena;
-	}
-
-	public int getUrtea() {
-		return urtea;
-	}
-
-	public String getFormatoa() {
-		return formatoa;
-	}
-
-	public float getIraupena() {
-		return iraupena;
-	}
-
-	public float getPrezioa() {
-		return prezioa;
-	}
-
-	public String getDisketxea() {
-		return disketxea;
-	}
-
-	public String getMediaCondition() {
-		return mediaCondition;
-	}
-
-	public List<String> getAbestiak() {
-		return abestiak;
-	}
-
-	// Setters
-	public void setId(int id) {
-		this.id = id;
-	}
-
 	public void setTaldea (Taldea taldea) {
 		this.taldea = taldea;
+	}
+
+	public String getIzena() {
+		return izena;
 	}
 
 	public void setIzena(String izena) {
 		this.izena = izena;
 	}
 
+	public int getUrtea() {
+		return urtea;
+	}
+
 	public void setUrtea(int urtea) {
 		this.urtea = urtea;
+	}
+
+	public String getFormatoa() {
+		return formatoa;
 	}
 
 	public void setFormatoa(String formatoa) {
 		this.formatoa = formatoa;
 	}
 
+	public float getIraupena() {
+		return iraupena;
+	}
+
 	public void setIraupena(float iraupena) {
 		this.iraupena = iraupena;
+	}
+
+	public float getPrezioa() {
+		return prezioa;
 	}
 
 	public void setPrezioa(float prezioa) {
 		this.prezioa = prezioa;
 	}
 
+	public String getDisketxea() {
+		return disketxea;
+	}
+
 	public void setDisketxea(String disketxea) {
 		this.disketxea = disketxea;
+	}
+
+	public String getMediaCondition() {
+		return mediaCondition;
 	}
 
 	public void setMediaCondition(String mediaCondition) {
 		this.mediaCondition = mediaCondition;
 	}
 
+	public List<String> getAbestiak() {
+		return abestiak;
+	}
+
 	public void setAbestiak(List<String> abestiak) {
 		this.abestiak = abestiak;
 	}
+
 	public String toString() {
 		return "Diskoa [id=" + id + ", taldea=" + taldea + ", izena=" + izena + ", urtea=" + urtea + ", formatoa="
 				+ formatoa + ", iraupena=" + iraupena + ", prezioa=" + prezioa + ", disketxea=" + disketxea
