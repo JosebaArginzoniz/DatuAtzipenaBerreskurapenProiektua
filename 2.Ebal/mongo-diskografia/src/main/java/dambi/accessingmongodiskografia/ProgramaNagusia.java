5704d77e0dbbeb9c35182fe7ada656df891844b4
JosebaArginzoniz <96474004+JosebaArginzoniz@users.noreply.github.com>
2023-02-17 01:23:55 +0100

    ProgramaNagusia

---
+++ 2.Ebal/mongo-diskografia/src/main/java/dambi/accessingmongodiskografia/ProgramaNagusia.java
@@ -0,0 +1,15 @@
+package dambi.accessingmongodiskografia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //Gauza asko honen atzien
+public class ProgramaNagusia {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramaNagusia.class, args);
+	}
+
+}
+
+//http://localhost:8080/swagger-ui/index.html
