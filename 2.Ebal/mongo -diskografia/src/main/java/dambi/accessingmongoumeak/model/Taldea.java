763526b84c286ddf34c55fb0414616b01408c04b
JosebaArginzoniz <96474004+JosebaArginzoniz@users.noreply.github.com>
2023-02-19 13:01:55 +0100

    Diskoak, Taldeak

--- 2.Ebal/mongo -diskografia/src/main/java/dambi/accessingmongoumeak/model/Taldea.java
+++ 2.Ebal/mongo -diskografia/src/main/java/dambi/accessingmongoumeak/model/Taldea.java
@@ -1,21 +1,65 @@
 package dambi.accessingmongoumeak.model;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-
 public class Taldea {
-     String izena;
-     String bateria;
-     String baxua;
-     String gitarraSolista;
-     String gitarra;
-     String ahotsa;
-
-    
-}
+     private String izena;
+     private String bateria;
+     private String baxua;
+     private String gitarraSolista;
+     private String gitarra;
+     private String ahotsa;
+ 
+     public String getIzena() {
+         return izena;
+     }
+ 
+     public void setIzena(String izena) {
+         this.izena = izena;
+     }
+ 
+     public String getBateria() {
+         return bateria;
+     }
+ 
+     public void setBateria(String bateria) {
+         this.bateria = bateria;
+     }
+ 
+     public String getBaxua() {
+         return baxua;
+     }
+ 
+     public void setBaxua(String baxua) {
+         this.baxua = baxua;
+     }
+ 
+     public String getGitarraSolista() {
+         return gitarraSolista;
+     }
+ 
+     public void setGitarraSolista(String gitarraSolista) {
+         this.gitarraSolista = gitarraSolista;
+     }
+ 
+     public String getGitarra() {
+         return gitarra;
+     }
+ 
+     public void setGitarra(String gitarra) {
+         this.gitarra = gitarra;
+     }
+ 
+     public String getAhotsa() {
+         return ahotsa;
+     }
+ 
+     public void setAhotsa(String ahotsa) {
+         this.ahotsa = ahotsa;
+     }
+ 
+     @Override
+     public String toString() {
+         return "Taldea{" + "izena=" + izena + ", bateria=" + bateria + ", baxua=" + baxua +
+                 ", gitarraSolista=" + gitarraSolista + ", gitarra=" + gitarra + ", ahotsa=" + ahotsa + "}";
+     }
+ }
+ 
